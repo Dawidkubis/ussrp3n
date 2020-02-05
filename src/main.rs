e126392f8f7223bf14cb2e54b5383d713faaaa0b
Dawid J. Kubis <dawid.kubis3@gmail.com>
2020-02-05 03:35:37 +0100

    first version

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,41 @@
-use structopt::StructOpt;
 use std::path::PathBuf;
+use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 struct Opt {
-	/// the post's headers
-	headers: PathBuf,
-	
-	/// the post's body, with {user} and {password} for replacement
-	body: String,
+    /// the post's headers
+    headers: PathBuf,
 
-	/// url to attack
-	url: String,
+    /// the post's body, with {user} and {password} for replacement
+    body: String,
 
-	/// users list
-	users: Option<PathBuf>,
-	
-	/// password dictionary
-	passwords: Option<PathBuf>,
+    /// url to attack
+    url: String,
 
-	/// proxy
-	proxy: Option<String>,
+    /// users list
+    users: Option<PathBuf>,
 
-	/// number of threads to run on
-	#[structopt(short, long, default_value = "1")]
-	threads:usize,
+    /// password dictionary
+    passwords: Option<PathBuf>,
+
+    /// proxy
+    #[structopt(short, long)]
+    proxy: Option<String>,
+
+    /// number of threads to run on
+    #[structopt(short, long, default_value = "1")]
+    threads: usize,
 }
 
 fn main() {
-	let opt = Opt::from_args();
-	let proxy = reqwest::Proxy::all("socks5h://localhost:9050").expect("tor isn't running");
-	let client = reqwest::Client::builder()
-		.proxy(proxy);
+    let opt = Opt::from_args();
+    let proxy = reqwest::Proxy::all("socks5h://127.0.0.1:9050").expect("tor isn't running");
+    let client = reqwest::blocking::Client::builder()
+        .proxy(proxy)
+        .build()
+        .unwrap();
 
+    let r = client.get("https://myexternalip.com/raw").send().unwrap();
+
+    println!("{:?}", r.text());
 }
