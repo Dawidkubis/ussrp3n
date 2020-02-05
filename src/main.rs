9be0a20785cad04483bc75692b27d2ead44afc1c
Dawid J. Kubis <dawid.kubis3@gmail.com>
2020-02-05 03:12:03 +0100

    building

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use structopt::StructOpt;
+use std::path::PathBuf;
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+	/// the post's headers
+	headers: PathBuf,
+	
+	/// the post's body, with {user} and {password} for replacement
+	body: String,
+
+	/// url to attack
+	url: String,
+
+	/// users list
+	users: Option<PathBuf>,
+	
+	/// password dictionary
+	passwords: Option<PathBuf>,
+
+	/// proxy
+	proxy: Option<String>,
+
+	/// number of threads to run on
+	#[structopt(short, long, default_value = "1")]
+	threads:usize,
+}
+
 fn main() {
-    println!("Hello, world!");
+	let opt = Opt::from_args();
+	let proxy = reqwest::Proxy::all("socks5h://localhost:9050").expect("tor isn't running");
+	let client = reqwest::Client::builder()
+		.proxy(proxy);
+
 }
