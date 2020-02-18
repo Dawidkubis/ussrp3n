69c931931dca9585e1d14b913c9c422adb7d3aae
Dawid J. Kubis <dawid.kubis3@gmail.com>
2020-02-18 22:22:44 +0100

    works... probably?

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use crate::Input::*;
 use anyhow::Result;
 use std::fs::read_to_string;
 use std::io::{self, Write};
@@ -8,116 +7,79 @@ use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 struct Opt {
-    /// the file with curl command
-    command: PathBuf,
+	/// the file with curl command
+	command: PathBuf,
 
-    /// users list
-    users: Option<PathBuf>,
+	/// users list
+	#[structopt(short, long)]
+	users: Option<PathBuf>,
 
-    /// password dictionary
-    passwords: Option<PathBuf>,
+	/// password dictionary
+	#[structopt(short, long)]
+	passwords: Option<PathBuf>,
 
-    /// number of threads to run on
-    #[structopt(short, long, default_value = "1")]
-    threads: usize,
-}
-
-#[derive(Debug, Clone)]
-enum Input {
-    File { count: usize, data: Vec<String> },
-    Io,
-}
-
-impl Input {
-    fn new(path: Option<impl AsRef<Path>>) -> Result<Self> {
-        match path {
-            Some(p) => {
-                let s = read_to_string(&p)?;
-                Ok(Self::File {
-                    count: 0,
-                    data: s.lines().map(String::from).collect(),
-                })
-            }
-            None => Ok(Self::Io),
-        }
-    }
-}
-
-impl Iterator for Input {
-    type Item = String;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        match self {
-            Self::File { count: c, data: d } => {
-                let r = d.get(*c).cloned();
-                *c += 1;
-                r
-            }
-            Self::Io => {
-                let mut input = String::new();
-                print!("//> ");
-                io::stdout().flush().ok()?;
-                io::stdin().read_line(&mut input).ok()?;
-                Some(input)
-            }
-        }
-    }
+	/// number of threads to run on
+	#[structopt(short, long, default_value = "1")]
+	threads: usize,
 }
 
 struct Main {
-    passwords: Input,
-    users: Input,
-    cmd: String,
-    count: usize,
+	passwords: Vec<String>,
+	users: Vec<String>,
+	cmd: String,
+	count: usize,
 }
 
 impl Main {
-    fn new(passwords: Option<PathBuf>, users: Option<PathBuf>, cmd: String) -> Self {
-        let passwords = Input::new(passwords).unwrap();
-        let users = Input::new(users).unwrap();
-        Self {
-            passwords,
-            users,
-            cmd,
-            count: 0,
-        }
-    }
+	fn new(passwords: PathBuf, users: PathBuf, cmd: String) -> Self {
+		let passwords:Vec<String> =	read_to_string(passwords)
+						.unwrap()
+						.lines()
+						.map(String::from)
+						.collect();
 
-    fn ussrp3n(&self, user: &str, pass: &str) -> Result<Output> {
-        Ok(Command::new("sh")
-            .arg("-c")
-            .arg(self.cmd.replace("{user}", user).replace("{password}", pass))
-            .output()?)
-    }
+		let users:Vec<String> =	read_to_string(users)
+						.unwrap()
+						.lines()
+						.map(String::from)
+						.collect();
+
+		Self {
+			passwords,
+			users,
+			cmd,
+			count: 0,
+		}
+	}
+
+	fn ussrp3n(&self, user: String, pass: String) -> Result<Output> {
+		Ok(Command::new("sh")
+			.arg("-c")
+			.arg(
+				self.cmd
+					.replace("{user}", user.as_ref())
+					.replace("{password}", pass.as_ref()),
+			)
+			.output()?)
+	}
 }
 
 impl Iterator for Main {
-    type Item = Result<Output>;
+	type Item = Output;
 
-    fn next(&mut self) -> Option<Self::Item> {
-        //TODO figure out what should be done here...
-        Some(match (&self.users, &self.passwords) {
-            (File { .. }, File { .. }) => self.ussrp3n("nig", "ger"),
-            (Io, Io) => self.ussrp3n(
-                &self.users.clone().next().unwrap(),
-                &self.passwords.next().unwrap(),
-            ),
-            (File { .. }, Io) => self.ussrp3n("nig", "ger"),
-            (Io, File { .. }) => self.ussrp3n("nig", "ger"),
-        })
-    } //function end
+	fn next(&mut self) -> Option<Self::Item> {
+			Some(self.ussrp3n(
+				self.users.get(self.count % self.users.len())?.to_string(),
+				self.passwords
+					.get(self.count / self.users.len() as usize)?.to_string()
+					,
+			).unwrap())
+	} //function end
 }
 
 fn main() {
-    let opt = Opt::from_args();
-    let cmd = read_to_string(opt.command).unwrap();
+	let opt = Opt::from_args();
+	let cmd = read_to_string(opt.command).unwrap();
 
-    let main = Main::new(opt.passwords, opt.users, cmd);
-
-    //let cmd = Command::new("sh").arg("-c").arg(command).output().unwrap();
-
-    //let ip = String::from_utf8(cmd.stdout).unwrap();
-    //let err = String::from_utf8(cmd.stderr).unwrap();
-
-    //println!("{}\n{}", err, ip);
+	let main = Main::new(opt.passwords.unwrap(), opt.users.unwrap(), cmd);
 }
